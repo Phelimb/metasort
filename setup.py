6e8ca3e4901739f04b88c47f89b2b8c069883fd0
Phelim Bradley <wave@phel.im>
2015-04-10 15:59:54 +0100

    need flask in setup.py for requests

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
  
 setup(
     name='metasort',
-    version='0.3.6.2',
+    version='0.3.6.3',
     packages=['metasort', ],
     license='MIT',
     url='http://github.com/phelimb/metasort',
     description='Filter reads based on taxonomy assignment from OneCodex.',    
     author='Phelim Bradley, Gil Goncalves',
     author_email='wave@phel.im, lursty@gmail.com',
-    install_requires=["requests==2.5.3","Biopython","onecodex"],
+    install_requires=["requests==2.5.3","Biopython","onecodex","Flask==0.10.1"],
     entry_points={
         'console_scripts': [
             'metasort = metasort.cli:main',
